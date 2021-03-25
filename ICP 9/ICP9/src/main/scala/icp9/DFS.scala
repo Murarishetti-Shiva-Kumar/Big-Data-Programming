bc5fbed501be89570d31985cc3d02a9febe8c4bf
Shiva Kumar Murarishetti <smpf4@mail.umkc.edu>
2021-03-25 00:27:25 -0500

    ICP 9 Code push

---
+++ ICP 9/ICP9/src/main/scala/icp9/DFS.scala
@@ -0,0 +1,39 @@
+package icp9
+
+import org.apache.spark._
+
+object DFS {
+  def main(args: Array[String]) {
+
+    System.setProperty("hadoop.home.dir","C:\\spark-3.1.1-bin-hadoop3.2")
+    val conf = new SparkConf().setAppName("merge sort").setMaster("local[*]")
+    // Create a Scala Spark Context.
+    val sc = new SparkContext(conf)
+    type Vertex = Int
+    type Graph = Map[Vertex, List[Vertex]]
+    //      val g: Graph=Map(1 -> List(2,4,6), 2 -> List(4,7))
+
+    val g: Graph=Map(1 -> List(7,9), 7 -> List(1,8),8 -> List(7,9), 9 -> List(1,8))
+
+    def DFS(start: Vertex, g: Graph): List[Vertex] = {
+      def DFS0(vertex: Vertex,visited: List[Vertex]): List[Vertex] = {
+        println(vertex)
+
+        println(visited)
+        if(visited.contains(vertex)) {
+          visited
+        }
+        else {
+          val newNeighbor = g(vertex).filterNot(visited.contains)
+          println(newNeighbor)
+          newNeighbor.foldLeft(vertex :: visited)((b, a) => DFS0(a, b))
+        }
+      }
+
+      DFS0(start, List()).reverse
+    }
+    val dfsresult=DFS(1,g)
+
+    println("DFS Output",dfsresult.mkString(","))
+  }
+}
---
+++ ICP 9/ICP9/src/main/scala/icp9/MergeSort.scala
@@ -0,0 +1,33 @@
+package icp9
+
+import org.apache.spark._
+
+object MergeSort {
+  def main(args: Array[String]) {
+
+    System.setProperty("hadoop.home.dir","C:\\spark-3.1.1-bin-hadoop3.2")
+    val conf = new SparkConf().setAppName("merge sort").setMaster("local[*]")
+    // Create a Scala Spark Context.
+    val sc = new SparkContext(conf)
+    def mergeSort(xs: List[Int]): List[Int] = {
+      val n = xs.length / 2
+      if (n == 0) xs
+      else
+      {
+        val (left, right) = xs splitAt(n)
+        merge(mergeSort(left), mergeSort(right))
+      }
+    }
+
+    println(mergeSort(List(7,1,90,3,5,8)))
+  }
+
+  def merge(xs: List[Int], ys: List[Int]): List[Int] =
+    (xs, ys) match {
+      case(Nil, ys) => ys
+      case(xs, Nil) => xs
+      case(x :: xs1, y :: ys1) =>
+        if (x < y) x::merge(xs1, ys)
+        else y :: merge(xs, ys1)
+    }
+}
